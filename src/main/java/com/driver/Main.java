ffab97bf6956c5207c839fd86dcec7a145a361c7
Aman  Gour <121440114+AmanGour214@users.noreply.github.com>
2023-01-13 04:48:05 -0800

    7 time change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,30 @@
 package com.driver;
-class A{
+// class A{
+//     public static String meth(){
+//         return "Invoking method from class A";
+//     }
+// }
+// class B extends A {
+//    public static String meth() {
+//         return "Method is overridden in Extendend class B";
+//     }
+//     //Method is overridden in Extendend class B"
+// }
+public class Main {
+    public static void main(String[] args) {
+        B b=new B();
+        System.out.println(b.meth());
+    }
+
+}
+ class A{
     public static String meth(){
         return "Invoking method from class A";
     }
+
 }
 class B extends A {
-   public static String meth() {
+    public static String meth(){
         return "Method is overridden in Extendend class B";
-    }
-    //Method is overridden in Extendend class B"
-}
-public class Main {
-    public static void main(String[] args){
-        B b=new B();
-        //String ans=b.meth();
-        System.out.println(b.meth());
     }
 }
