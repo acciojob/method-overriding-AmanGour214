5230c859ac8281260375264d7f4c09787ed87eff
Aman Gour <amangour562@gmail.com>
2023-01-13 17:57:06 -0800

    5rd  time change for error resolve

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,13 +17,10 @@ class B extends A {
 
 
 public class Main {
-
-
-
     public static void main(String[] args){
         B b=new B();
-        b.meth();
-        //System.out.println(ans);
+        String ans=b.meth();
+        System.out.println(ans);
 
     }
   
