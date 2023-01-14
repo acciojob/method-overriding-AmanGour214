0b570efaf5414179af22c444fed5d1e1c4fe0371
Aman Gour <amangour562@gmail.com>
2023-01-13 17:13:02 -0800

    first time written code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+
+    static class A{
+        String meth(String s){
+            return "Invoking method from class A";
+        }
+
+    }
+   static class B extends A{
+       @Override
+       String meth(String s) {
+           return "Method is overridden in Extendend class B";
+       }
+   }
+
+
+    public static void main(String[] args){
+        B b=new B();
+        b.meth("aman");
+       // System.out.println(ans);
+
+    }
   
 }
