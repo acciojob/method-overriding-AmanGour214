413bf4e77ad86167f6dfc62183bf17ea54a66728
Aman Gour <amangour562@gmail.com>
2023-01-13 18:09:33 -0800

    6rd  time change for error resolve

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,19 @@
 package com.driver;
-
-
 class A{
-    public String meth(){
+    public static String meth(){
         return "Invoking method from class A";
     }
-
 }
 class B extends A {
-    @Override
-   public String meth() {
+   public static String meth() {
         return "Method is overridden in Extendend class B";
     }
     //Method is overridden in Extendend class B"
 }
-
-
 public class Main {
     public static void main(String[] args){
         B b=new B();
         String ans=b.meth();
         System.out.println(ans);
-
     }
-  
 }
