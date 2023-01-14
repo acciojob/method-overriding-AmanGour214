94efb34db085ef4f18f1a3ed431cd6b78c5c5a07
Aman Gour <amangour562@gmail.com>
2023-01-13 17:17:17 -0800

    second time change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,13 +2,13 @@ package com.driver;
 
 public class Main {
 
-    static class A{
+    public static class A{
         String meth(String s){
             return "Invoking method from class A";
         }
 
     }
-   static class B extends A{
+   public static class B extends A{
        @Override
        String meth(String s) {
            return "Method is overridden in Extendend class B";
